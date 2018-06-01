5685f32a4bba89dcf958432e3645390ebbef23ae
Kao <floconetflanelle@gmail.com>
2018-06-02 01:31:06 +0200

    Initial commit. Don't pay attention to eso_server nor discord.js files.

---
+++ discord.js
@@ -0,0 +1,12 @@
+const DiscordJS = require('discord.js');
+
+class Discord {
+
+    constructor() {
+        this.client.on('ready', () => {console.log(`Loggeds in as ${client.user.tag}!`);
+            });
+
+
+        client.login('MzQ5ODU1NzcyNTM4MTc1NDg5.DeyxAg.09EkuUIcf1SdLAQB8anNujAvxLk');
+    }
+}
