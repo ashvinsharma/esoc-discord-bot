5edb0a85a7c851161636336a44a66ea4b19a7975
ashvinsharma <ashvinsharma97@gmail.com>
2018-06-22 17:57:18 +0530

    :sunglasses: All fixing done. Besides DB everything is ready to go live.

--- twitch.js
+++ twitch.js
@@ -6,18 +6,12 @@ const prefixJsonData = 'user_';
 const apiTwitch = 'https://api.twitch.tv/helix/';
 const apiUsers = 'users?id=';
 const apiStreams = 'streams?game_id=10819';
-const schedulesURL = 'http://eso-community.net/app.php/current-streams-schedule';
 const optionsTwitch = {
   headers: {
     'Client-ID': clientID,
   },
   json: true,
   timeout: '30',
-};
-const optionsSchedules = {
-  uri: schedulesURL,
-  timeout: '30',
-  json: true,
 };
 const twitchUrl = 'https://www.twitch.tv/';
 const GRAY = 0x4f545c;
@@ -27,29 +21,26 @@ const GOLD_COUNT = 25;
 class Twitch {
   static async getUserFromCache(userID) {
     const user = prefixJsonData + userID;
-    let writer = null;
     let file = fs.readFileSync('user.json', 'utf8');
     file = JSON.parse(file);
     if (file[user]) {
       return file[user];
     }
-    writer = await this.writeToCache(userID, user, file);
-    return writer;
+    return this.writeToCache(userID, user, file);
   }
 
-  static async writeToCache(userID, user, file) {
+  static async writeToCache(userID, user) {
     let data;
-    let req;
     const temp = null;
     try {
-      req = request.get(apiTwitch + apiUsers + userID.toString(), optionsTwitch);
-      const res = await req;
+      const res = await request.get(apiTwitch + apiUsers + userID.toString(), optionsTwitch);
       data = res.data[0]; // We are doing this
       data = JSON.stringify(data, ['id', 'login', 'display_name']); // to keep only
       data = JSON.parse(data); // the data we need.
-      file[user] = data;
-      file = JSON.stringify(file, null, '  '); // Pretty stringify the object in JSON.
-      fs.writeFileSync('user.json', file); // We write back the object to the file.
+      let info = {};
+      info[user] = data;
+      info = JSON.stringify(info, null, '  '); // Pretty stringify the object in JSON.
+      fs.writeFileSync('user.json', info); // We write back the object to the file.
       return data;
     } catch (e) {
       console.error(`${new Date()} `, e);
@@ -66,10 +57,7 @@ class Twitch {
     } else {
       try {
         const user = prefixJsonData + userID;
-        const file = {};
-        let writer;
-        writer = await this.writeToCache(userID, user, file);
-        userData = writer;
+        userData = await this.writeToCache(userID, user);
       } catch (e) {
         console.error(`${new Date()} `, e);
       }
@@ -81,9 +69,10 @@ class Twitch {
     const res = await request(apiTwitch + apiStreams, optionsTwitch);
     const streams = res.data;
     const users = {};
-    for (const stream of streams) {
-      users[`user_${stream.user_id}`] = await this.getUser(stream.user_id);
-    }
+    await Promise.all(streams.map(((stream) => {
+      users[`user_${stream.user_id}`] = this.getUser(stream.user_id);
+    })))
+      .catch(e => console.error(`${new Date()} `, e));
     return {
       streams,
       users,
