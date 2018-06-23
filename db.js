e620535ed260eb481136d1f12977d92f8973d451
ashvinsharma <ashvinsharma97@gmail.com>
2018-06-24 03:10:08 +0530

    Did something that I don't remember now :thinking:

--- db.js
+++ db.js
@@ -15,12 +15,3 @@ con.on('connection', (connection) => {
 });
 
 module.exports = con;
-
-// async function random() {
-//   const getMap = 'SELECT esoc.maps.ID, esoc.maps.DisplayName, esoc.maps.MiniMapUrl, ' +
-//     'phpBB.p_users.username, esoc.maps.TPs, esoc.maps.Natives, esoc.maps.Outlaws, ' +
-//     'esoc.maps.Date, esoc.maps.GameType FROM esoc.maps LEFT JOIN phpBB.p_users ON ' +
-//     'esoc.maps.Author = phpBB.p_users.user_id';
-//   const [rows, fields] = await con.execute(getMap);
-//   console.log(JSON.stringify(rows));
-// }
