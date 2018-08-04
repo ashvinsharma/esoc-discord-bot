e26f748d845dbee69ef76736718b999660cfde69
ashvinsharma <ashvinsharma97@gmail.com>
2018-08-05 05:18:13 +0530

    Implement Player info feature that fetches data from agecomm API

--- constants.js
+++ constants.js
@@ -1,3 +1,15 @@
+const GOLD = 0xffa500;
+const GOLD_COUNT = 25;
+const GRAY = 0x4f545c;
+const GREEN = 3842351; // decimal representation of hexa color
+// TODO: change player name
+const ESO_QUERY = 'http://www.agecommunity.com/query/query.aspx?g=age3y&name=arg1&md=user';
+const ESOC = 'http://eso-community.net';
+const ESOC_LOBBIES_URI = '/assets/patch/api/lobbies.json';
+const ESOC_SUPREMACY_STANDARD_LADDER = `${ESOC}/ladder.php?patch=official&type=treaty&mode=overall&player=`;
+const ESOC_SUPREMACY_TREATY_LADDER = `${ESOC}/ladder.php?patch=esoc&type=supremacy&mode=overall&player=`;
+const ESOC_PATCH_ICON = '/images/aoe3/patch-esoc-icon.png';
+const ESOC_PATCH_EMBED_COLOR = 0xc32025;
 const MAPS = [
   {
     mapName: 'Unknown',
@@ -45,16 +57,11 @@ const MAPS = [
     MiniMapUrl: '/images/aoe3/maps/standard_maps.png',
   },
 ];
-const UPDATE_TWITCH_INTERVAL = 60000; // milli seconds
-const UPDATE_INTERVAL_ESOC = 15000; // milli seconds
-const ESOC = 'http://eso-community.net';
-const ESOC_LOBBIES_URI = '/assets/patch/api/lobbies.json';
-const ESOC_SUPREMACY_STANDARD_LADDER = `${ESOC}/ladder.php?patch=official&type=treaty&mode=overall&player=`;
-const ESOC_SUPREMACY_TREATY_LADDER = `${ESOC}/ladder.php?patch=esoc&type=supremacy&mode=overall&player=`;
-const ESOC_PATCH_ICON = '/images/aoe3/patch-esoc-icon.png';
-const ESOC_PATCH_EMBED_COLOR = 0xc32025;
+const MAPS_QUERY = 'SELECT a.alias_id, a.name AS map_name, m.*, p.username FROM esoc.maps_alias a '
+  + 'INNER JOIN esoc.maps m ON m.ID = a.alias_id '
+  + 'LEFT JOIN phpBB.p_users p ON p.user_id = m.Author';
+const RED = 0xB22222;
 const SCENARIO_IMAGE = '/images/aoe3/maps/scenario.png';
-const UNKNOWN_MAP_IMAGE = '/images/aoe3/maps/unknown.png';
 const TREATY_PATCH_ICON = '/images/aoe3/patch-treaty-icon.png';
 const TREATY_PATCH_EMBED_COLOR = 0x0378c0;
 const TWITCH = 'https://www.twitch.tv/';
@@ -69,37 +76,37 @@ const TWITCH_OPTIONS = {
   json: true,
   timeout: '30',
 };
-const MAPS_QUERY = 'SELECT a.alias_id, a.name AS map_name, m.*, p.username FROM esoc.maps_alias a '
-  + 'INNER JOIN esoc.maps m ON m.ID = a.alias_id '
-  + 'LEFT JOIN phpBB.p_users p ON p.user_id = m.Author';
-const GRAY = 0x4f545c;
-const GOLD = 0xffa500;
-const GOLD_COUNT = 25;
+const UNKNOWN_MAP_IMAGE = '/images/aoe3/maps/unknown.png';
+const UPDATE_TWITCH_INTERVAL = 60000; // milli seconds
+const UPDATE_INTERVAL_ESOC = 15000; // milli seconds
 const XP_MOD_EMBED_COLOR = 0xc27c0e;
 
 module.exports = {
-  MAPS,
-  MAPS_QUERY,
-  UPDATE_TWITCH_INTERVAL,
-  UPDATE_INTERVAL_ESOC,
+  GOLD,
+  GOLD_COUNT,
+  GRAY,
+  GREEN,
+  ESO_QUERY,
   ESOC,
   ESOC_LOBBIES_URI,
   ESOC_PATCH_ICON,
   ESOC_PATCH_EMBED_COLOR,
   ESOC_SUPREMACY_TREATY_LADDER,
   ESOC_SUPREMACY_STANDARD_LADDER,
+  MAPS,
+  MAPS_QUERY,
+  RED,
+  SCENARIO_IMAGE,
   TREATY_PATCH_ICON,
   TREATY_PATCH_EMBED_COLOR,
-  GRAY,
-  GOLD,
-  GOLD_COUNT,
   TWITCH,
   TWITCH_API_URI,
   TWITCH_CLIENT_ID,
   TWITCH_API_USERS_URI,
   TWITCH_API_STREAMS_URI,
   TWITCH_OPTIONS,
-  SCENARIO_IMAGE,
+  UPDATE_TWITCH_INTERVAL,
+  UPDATE_INTERVAL_ESOC,
   UNKNOWN_MAP_IMAGE,
   XP_MOD_EMBED_COLOR,
 };
