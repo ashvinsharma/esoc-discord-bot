423646f821519bfeba28570730a89e66f64e4bf3
Kao <floconetflanelle@gmail.com>
2018-09-02 21:36:39 +0000

    Revert "Merge branch 'test/disable-db-features' into 'master'"
    This reverts merge request !80

--- db.js
+++ db.js
@@ -1,21 +1,21 @@
-// const mysql = require('mysql2/promise');
-// const { log, logError } = require('./logger');
+const mysql = require('mysql2/promise');
+const { log, logError } = require('./logger');
 
-// const con = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-// });
+const con = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
 
-// // Attempt to catch disconnects
-// con.on('connection', (connection) => {
-//   log('Database Connection established');
-//   connection.on('error', (err) => {
-//     logError(`Database error. MySQL error: ${err.code}`);
-//   });
-//   connection.on('close', (err) => {
-//     logError(`Database connection closed. ${err}`);
-//   });
-// });
+// Attempt to catch disconnects
+con.on('connection', (connection) => {
+  log('Database Connection established');
+  connection.on('error', (err) => {
+    logError(`Database error. MySQL error: ${err.code}`);
+  });
+  connection.on('close', (err) => {
+    logError(`Database connection closed. ${err}`);
+  });
+});
 
-// module.exports = con;
+module.exports = con;
